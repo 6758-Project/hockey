61bb9bab6c5e183bcd99751ef813c519d53f3f4a
Jake White <jake@riskfuel.com>
2021-09-26 14:39:45 -0400

    separated game id generation logic into standalone functions

--- src/data/download_data.py
+++ src/data/download_data.py
@@ -9,7 +9,43 @@ import requests
 from typing import List
 
 
-game_url_from_id = lambda game_id: f"https://statsapi.web.nhl.com/api/v1/game/{game_id}/feed/live/"
+def generate_regular_season_game_ids(season: int):
+    """ For an NHL season starting in year SEASON, return all regular season game IDs.
+
+        See the unofficial documentation for information about game ID conventions:
+        https://gitlab.com/dword4/nhlapi/-/blob/master/stats-api.md#game-ids
+    """
+    regular_season_games = 1230 if season <= 2016 else 1271
+
+    regular_game_ids = []
+    for game_num in range(1, regular_season_games):
+        game_num_padded = str(game_num).zfill(4)
+        regular_game_id = str(season) + "02" + game_num_padded
+        regular_game_ids.append(regular_game_id)
+
+    return regular_game_ids
+
+
+def generate_postseason_game_ids(season: int):
+    """ For an NHL season starting in year SEASON, returns all postseason game IDs.
+
+        See the unofficial documentation for information about game ID conventions:
+        https://gitlab.com/dword4/nhlapi/-/blob/master/stats-api.md#game-ids
+    """
+    postseason_game_ids = []
+    for rd in range(1, 4+1):
+        series_per_round = [8, 4, 2, 1][rd-1]
+        for series_num in range(1, series_per_round+1):
+            for game_num in range(1,7+1):
+                playoff_game_id = str(season) + str("03") + str(series_num)+str(game_num)
+                postseason_game_ids.append(playoff_game_id)
+
+    return postseason_game_ids
+
+
+def get_game_url(game_id: str):
+    """ Returns an NHL stats API URL based on an NHL GAME_ID """
+    return f"https://statsapi.web.nhl.com/api/v1/game/{game_id}/feed/live/"
 
 
 def download_games(game_ids: List, datadir: os.PathLike):
@@ -19,7 +55,7 @@ def download_games(game_ids: List, datadir: os.PathLike):
         Logs warnings if any game ID are misspecified.
     """
     for game_id in game_ids:
-        response = requests.get(game_url_from_id(game_id))
+        response = requests.get(get_game_url(game_id))
 
         if response.status_code == 404:
             logging.warning(f"No data returned for game ID {game_id} (404), so skipping")
@@ -29,29 +65,27 @@ def download_games(game_ids: List, datadir: os.PathLike):
                 f.write(response.content)
 
 
+def create_nhl_data_directory(datadir: os.PathLike, seasons: List[int]):
+    """ Creates a directory tree for NHL data.
+
+        Pattern: datadir/season/subseason_code/
+    """
+    for season in seasons:
+        os.makedirs(os.path.join(datadir, str(season), "02"), exist_ok=True)
+        os.makedirs(os.path.join(datadir, str(season), "03"), exist_ok=True)
+
+
 def main(args):
+    create_nhl_data_directory(args.datadir, args.seasons)
+
     requested_game_ids = []
-
     for season in args.seasons:
-        regular_season_games = 1230 if season <= 2016 else 1271
 
-        # regular season
         if not args.postseason_only:
-            for game_num in range(1, regular_season_games):
-                game_num_padded = str(game_num).zfill(4)
-                regular_game_id = str(season) + "02" + game_num_padded
-                requested_game_ids.append(regular_game_id)
+            requested_game_ids.extend(generate_regular_season_game_ids(season))
 
         if not args.regular_season_only:
-            for rd in range(1, 4+1):
-                series_per_round = [8, 4, 2, 1][rd-1]
-                for series_num in range(1, series_per_round+1):
-                    for game_num in range(1,7+1):
-                        playoff_game_id = str(season) + str("03") + str(series_num)+str(game_num)
-                        requested_game_ids.append(playoff_game_id)
-
-        os.makedirs(os.path.join(args.datadir, str(season), "02"), exist_ok=True)
-        os.makedirs(os.path.join(args.datadir, str(season), "03"), exist_ok=True)
+            requested_game_ids.extend(generate_postseason_game_ids(season))
 
     download_games(requested_game_ids, args.datadir)
 
