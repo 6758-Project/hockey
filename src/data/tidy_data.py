b70ddcf06ac8609d65ff0ff3ba46ac6a9573cb26
Jake White <jake@riskfuel.com>
2021-11-07 15:29:01 -0500

    expanded tidy_data to include all event types

--- src/data/tidy_data.py
+++ src/data/tidy_data.py
@@ -2,9 +2,12 @@
 Extracts the events information from the downloaded data with the NHL API.
 """
 import argparse
+import glob
 import os
 import json
 import logging
+
+import numpy as np
 import pandas as pd
 
 from nhl_proj_tools.data_utils import (
@@ -17,7 +20,7 @@ def flip_coord_to_one_side(game_events_df, right_team, left_team):
     """
     Flip the (x,y) coordinates of the shots events to the right side of the rink for both teams
 
-    :param pd.DataFrame game_events_df: all the game events 
+    :param pd.DataFrame game_events_df: all the game events
     :param str right_team: the team that started the game on the right side of the rink
     :param str left_team: the team that started the game on the left side of the rink
     :return: a dataframe of the game events data after updating
@@ -36,6 +39,10 @@ def flip_coord_to_one_side(game_events_df, right_team, left_team):
             game_events_df.at[idx, "coordinate_x"] = row["coordinate_x"] * -1
             game_events_df.at[idx, "coordinate_y"] = row["coordinate_y"] * -1
     return game_events_df
+
+
+def add_milestone2_metrics(events):
+    return events  # TODO
 
 
 def parse_game_data(game_id: str, game_data: dict):
@@ -57,121 +64,118 @@ def parse_game_data(game_id: str, game_data: dict):
 
     # loop over all events in the game
     for event in game_data["liveData"]["plays"]["allPlays"]:
-
         # get the event type
-        event_result_info = event["result"]
-        event_type_id = event_result_info["eventTypeId"]
+        event_result_info = event.get("result", None)
+        event_type_id = event_result_info.get("eventTypeId", None)
         # a set for all unique events in the game
         event_types.add(event_type_id)
 
-        # for this milestone we are interested in SHOT or GOAL types
-        if event_type_id == "SHOT" or event_type_id == "GOAL":
-            event_code = event_result_info["eventCode"]
-            event_desc = event_result_info["description"]
-            # doesn't exist in other event types
-            if "secondaryType" in event_result_info.keys():
-                event_secondary_type = event_result_info["secondaryType"]
-            else:
-                event_secondary_type = None
+        event_code = event_result_info.get("eventCode", None)
+        event_desc = event_result_info.get("description", None)
+        # doesn't exist in other event types
+        if "secondaryType" in event_result_info.keys():
+            event_secondary_type = event_result_info.get("secondaryType", None)
+        else:
+            event_secondary_type = None
 
-            # event information
-            event_about_info = event["about"]
-            event_id = event_about_info["eventId"]
-            # event index inside the allPlays in the json file
-            event_index = event_about_info["eventIdx"]
-            period_num = event_about_info["period"]
-            period_type = event_about_info["periodType"]
-            event_date = event_about_info["dateTime"]
-            event_time = event_about_info["periodTime"]
-            event_time_remaining = event_about_info["periodTimeRemaining"]
-            event_goals_home = event_about_info["goals"]["home"]
-            event_goals_away = event_about_info["goals"]["away"]
+        # event information
+        event_about_info = event.get("about", None)
+        event_id = event_about_info.get("eventId", None)
+        # event index inside the allPlays in the json file
+        event_index = event_about_info.get("eventIdx", None)
+        period_num = event_about_info.get("period", None)
+        period_type = event_about_info.get("periodType", None)
+        event_date = event_about_info.get("dateTime", None)
+        event_time = event_about_info.get("periodTime", None)
+        event_time_remaining = event_about_info.get("periodTimeRemaining", None)
+        event_goals_home = event_about_info["goals"]["home"]
+        event_goals_away = event_about_info["goals"]["away"]
 
-            # shooting/scoring team information
-            shooter_team_info = event["team"]
-            shooter_team_id = shooter_team_info["id"]
-            shooter_team_name = shooter_team_info["name"]
-            shooter_team_code = shooter_team_info["triCode"]
+        # shooting/scoring team information
+        # import pdb; pdb.set_trace()
+        shooter_team_info = event.get("team", None)
+        shooter_team_id = shooter_team_info.get("id", None) if shooter_team_info else None
+        shooter_team_name = shooter_team_info.get("name", None) if shooter_team_info else None
+        shooter_team_code = shooter_team_info.get("triCode", None) if shooter_team_info else None
 
-            # players information (i.e. the shooter/scorer and the goalie)
-            # Shooter/scorer information
-            players_info = event["players"]
-            shooter_info = players_info[0]["player"]
-            shooter_role = players_info[0]["playerType"]
-            shooter_id = shooter_info["id"]
-            shooter_name = shooter_info["fullName"]
+        # players information (i.e. the shooter/scorer and the goalie)
+        # Shooter/scorer information
+        players_info = event.get("players", None)
+        shooter_info = players_info[0].get("player", None) if players_info else None
+        shooter_role = players_info[0].get("playerType", None) if players_info else None
+        shooter_id = shooter_info.get("id", None) if shooter_info else None
+        shooter_name = shooter_info.get("fullName", None) if shooter_info else None
 
-            # Goalie information
-            # GOAL event has from 2 to 4 players info: scorer, goalie and up to two assists
-            # SHOOT event has 2 players info: shooter and goalie
-            # in both cases the goalie is always at the end of the list
-            goalie_info = players_info[-1]["player"]
-            goalie_role = players_info[-1]["playerType"]
-            goalie_id = goalie_info["id"]
-            goalie_name = goalie_info["fullName"]
+        # Goalie information
+        # GOAL event has from 2 to 4 players info: scorer, goalie and up to two assists
+        # SHOOT event has 2 players info: shooter and goalie
+        # in both cases the goalie is always at the end of the list
+        goalie_info = players_info[-1].get("player", None) if players_info else None
+        goalie_role = players_info[-1].get("playerType", None) if players_info else None
+        goalie_id = goalie_info.get("id", None) if goalie_info else None
+        goalie_name = goalie_info.get("fullName", None) if goalie_info else None
 
-            # info specific to GOAL events
-            empty_net = None
-            game_winning_goal = None
-            strength_name = None
-            strength_code = None
-            if event_type_id == "GOAL":
-                if "emptyNet" in event_result_info.keys():
-                    empty_net = event_result_info["emptyNet"]
-                if "gameWinningGoal" in event_result_info.keys():
-                    game_winning_goal = event_result_info["gameWinningGoal"]
-                if "strength" in event_result_info.keys():
-                    strength_name = event_result_info["strength"]["name"]
-                    strength_code = event_result_info["strength"]["code"]
+        # info specific to GOAL events
+        empty_net = None
+        game_winning_goal = None
+        strength_name = None
+        strength_code = None
+        if event_type_id == "GOAL":
+            if "emptyNet" in event_result_info.keys():
+                empty_net = event_result_info.get("emptyNet", None)
+            if "gameWinningGoal" in event_result_info.keys():
+                game_winning_goal = event_result_info.get("gameWinningGoal", None)
+            if "strength" in event_result_info.keys():
+                strength_name = event_result_info["strength"]["name"]
+                strength_code = event_result_info["strength"]["code"]
 
-            # (x,y) coordinates of the event
-            coord_info = event["coordinates"]
+        # (x,y) coordinates of the event
+        coord_info = event.get("coordinates", None)
 
-            coord_x, coord_y = None, None
+        coord_x, coord_y = None, None
 
-            if "x" in coord_info.keys():
-                coord_x = coord_info["x"]
-            if "y" in coord_info.keys():
-                coord_y = coord_info["y"]
+        if coord_info and "x" in coord_info.keys():
+            coord_x = coord_info["x"]
+        if coord_info and "y" in coord_info.keys():
+            coord_y = coord_info["y"]
 
-            event_entry = {
-                "id": event_id,
-                "event_index": event_index,
-                "game_id": game_id,
-                "home_team": home_team,
-                "away_team": away_team,
-                "type": event_type_id,
-                "secondary_type": event_secondary_type,
-                "description": event_desc,
-                "code": event_code,
-                "period": period_num,
-                "period_type": period_type,
-                "time": event_time,
-                "time_remaining": event_time_remaining,
-                "date": event_date,
-                "goals_home": event_goals_home,
-                "goals_away": event_goals_away,
-                "shooter_team_id": shooter_team_id,
-                "shooter_team_name": shooter_team_name,
-                "shooter_team_code": shooter_team_code,
-                "shooter_name": shooter_name,
-                "shooter_id": shooter_id,
-                "goalie_name": goalie_name,
-                "goalie_id": goalie_id,
-                "is_empty_net": empty_net,
-                "is_winning_goal": game_winning_goal,
-                "strength_name": strength_name,
-                "strength_code": strength_code,
-                "coordinate_x": coord_x,
-                "coordinate_y": coord_y,
-            }
-            events.append(event_entry)
+        event_entry = {
+            "id": event_id,
+            "event_index": event_index,
+            "game_id": game_id,
+            "home_team": home_team,
+            "away_team": away_team,
+            "type": event_type_id,
+            "secondary_type": event_secondary_type,
+            "description": event_desc,
+            "code": event_code,
+            "period": period_num,
+            "period_type": period_type,
+            "time": event_time,
+            "time_remaining": event_time_remaining,
+            "date": event_date,
+            "goals_home": event_goals_home,
+            "goals_away": event_goals_away,
+            "shooter_team_id": shooter_team_id,
+            "shooter_team_name": shooter_team_name,
+            "shooter_team_code": shooter_team_code,
+            "shooter_name": shooter_name,
+            "shooter_id": shooter_id,
+            "goalie_name": goalie_name,
+            "goalie_id": goalie_id,
+            "is_empty_net": empty_net,
+            "is_winning_goal": game_winning_goal,
+            "strength_name": strength_name,
+            "strength_code": strength_code,
+            "coordinate_x": coord_x,
+            "coordinate_y": coord_y,
+        }
+        events.append(event_entry)
 
-    # calculate the median of the x_coordinate to see where did the teams start from (left or right)
     events_df = pd.DataFrame(events)
 
-    if events_df.empty == False:
-
+    # calculate the median of the x_coordinate to see where did the teams start from (left or right)
+    if not events_df.empty:
         median_df = (
             events_df[(events_df["period"] == 1)]
             .groupby(["shooter_team_name", "home_team"])[
@@ -180,6 +184,7 @@ def parse_game_data(game_id: str, game_data: dict):
             .median()
             .reset_index()
         )
+        import pdb; pdb.set_trace()
         for idx, row in median_df.iterrows():
             if row["home_team"] == row["shooter_team_name"]:
                 if (
@@ -188,6 +193,8 @@ def parse_game_data(game_id: str, game_data: dict):
                     events_df = flip_coord_to_one_side(events_df, home_team, away_team)
                 else:
                     events_df = flip_coord_to_one_side(events_df, away_team, home_team)
+
+        events_df = add_milestone2_metrics(events_df)
 
     return events_df
 
@@ -248,10 +255,15 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    # create the cleaned datadir if not exist
-    os.makedirs(args.clean_datadir, exist_ok=True)
+    # empty cleaned datadir if exists; otherwise create it
+    if os.path.exists(args.clean_datadir):
+        files = glob.glob(args.clean_datadir+"*")
+        for f in files:
+            os.remove(f)
+    else:
+        os.makedirs(args.clean_datadir, exist_ok=True)
 
-    for season in [2016, 2017, 2018, 2019, 2020]:
+    for season in range(2015, 2015+1): #, 2019+1):
         game_ids = generate_regular_season_game_ids(
             season
         ) + generate_postseason_game_ids(season)
