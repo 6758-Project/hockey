2527a4da2329516692a27db36c488e0459b98dd8
Sara Ebrahim <sara.elkafrawy@gmail.com>
2021-11-26 22:31:26 -0500

    feature selection with lasso and SHAP + black and pylint

--- src/training/utils.py
+++ src/training/utils.py
@@ -3,52 +3,83 @@
 import logging
 import pickle
 import warnings
-logging.basicConfig(level = logging.INFO)
+import sklearn
 
 from comet_ml import Experiment
-import sklearn
+
+logging.basicConfig(level=logging.INFO)
 
 
 # Defined Constants
 
 EXP_KWARGS = {
-    'project_name': 'ift6758-hockey',
-    'workspace': "tim-k-lee",
-    'auto_param_logging': True
+    "project_name": "ift6758-hockey",
+    "workspace": "tim-k-lee",
+    "auto_param_logging": True,
 }
 
-LABEL_COL = 'is_goal'
+LABEL_COL = "is_goal"
 
 INFREQUENT_STOPPAGE_EVENTS = [
-    'PERIOD_START', 'PERIOD_READY', 'PERIOD_END', 'SHOOTOUT_COMPLETE', 'PERIOD_OFFICIAL', 'GAME_OFFICIAL', 'PENALTY', 'GOAL', 'CHALLENGE'
+    "PERIOD_START",
+    "PERIOD_READY",
+    "PERIOD_END",
+    "SHOOTOUT_COMPLETE",
+    "PERIOD_OFFICIAL",
+    "GAME_OFFICIAL",
+    "PENALTY",
+    "GOAL",
+    "CHALLENGE",
 ]
 
 KNOWN_NON_TRAIN_COLS = [
-    'game_id', 'event_index', 'description',
-    'game_sec', 'time', 'time_remaining', 'date', 'prev_event_time_diff',
-    'is_goal'
+    "game_id",
+    "event_index",
+    "description",
+    "game_sec",
+    "time",
+    "time_remaining",
+    "date",
+    "prev_event_time_diff",
+    "is_goal",
 ]
 
 TRAIN_COLS_BASIC = [
-    'period', 'goals_home', 'goals_away',
-    'shooter_id', 'coordinate_x', 'coordinate_y', 'distance_from_net',
-    'angle'
+    "period",
+    "goals_home",
+    "goals_away",
+    "shooter_id",
+    "coordinate_x",
+    "coordinate_y",
+    "distance_from_net",
+    "angle",
 ]
 
 # TODO: throw out is_empty_net + associated rows?
 # see https://piazza.com/class/krgt4sfrgfp278?cid=255
 TRAIN_COLS_PART_4 = [
-    'game_sec', 'period',
-    'secondary_type',
-    'coordinate_x', 'coordinate_y', 'distance_from_net', 'angle',
-    'prev_event_type', 'angle_between_prev_event', 'distance_from_prev_event', 'prev_event_time_diff',
-    'speed', 'is_rebound', 'rebound_angle', 'is_empty_net'
+    "game_sec",
+    "period",
+    "secondary_type",
+    "coordinate_x",
+    "coordinate_y",
+    "distance_from_net",
+    "angle",
+    "prev_event_type",
+    "angle_between_prev_event",
+    "distance_from_prev_event",
+    "prev_event_time_diff",
+    "speed",
+    "is_rebound",
+    "rebound_angle",
+    "is_empty_net",
 ]  # 'period_type', 'shooter_team_name', 'shooter_id', 'goalie_name',
 
 RANDOM_STATE = 1729
 
 
 # Functions
+
 
 def clf_performance_metrics(y_true, y_pred, y_proba, verbose=False):
     """Generate the model performance metrics.
@@ -61,7 +92,7 @@ def clf_performance_metrics(y_true, y_pred, y_proba, verbose=False):
     """
 
     with warnings.catch_warnings():
-        warnings.simplefilter('ignore', sklearn.exceptions.UndefinedMetricWarning)
+        warnings.simplefilter("ignore", sklearn.exceptions.UndefinedMetricWarning)
         acc = sklearn.metrics.accuracy_score(y_true, y_pred)
         f1 = sklearn.metrics.f1_score(y_true, y_pred)
         precision = sklearn.metrics.precision_score(y_true, y_pred)
@@ -76,14 +107,12 @@ def clf_performance_metrics(y_true, y_pred, y_proba, verbose=False):
         logging.info("Precision score is {:6.3f}".format(precision))
         logging.info("Recall score is {:6.3f}".format(recall))
 
-    res = {
-        'accuracy': acc, 'f1_score': f1, 'precision': precision, 'recall': recall
-    }
+    res = {"accuracy": acc, "f1_score": f1, "precision": precision, "recall": recall}
 
     return res
 
 
-def log_experiment(params, perf_metrics, X_train, exp_name=None, pickle_path=None):
+def log_experiment(params, perf_metrics, X_train, exp_name=None):
     """Log model parameters and performance metrics to Comet.
     Args:
         params: Model parameters
@@ -118,7 +147,7 @@ def register_model(clf, comet_exp, pickle_path=None):
         logging.info("Parameters missing, cannot save files")
     else:
         logging.info("Saving model to pickle file")
-        with open(pickle_path,"wb") as f:
+        with open(pickle_path, "wb") as f:
             pickle.dump(clf, f, pickle.HIGHEST_PROTOCOL)
 
         comet_exp.log_model(name=comet_exp.get_name(), file_or_folder=pickle_path)
